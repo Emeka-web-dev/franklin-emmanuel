bdaece85a3e6961fc05ef30d68a4c5fe7e8c4364
Emeka-web-dev <chukwurex042@gmail.com>
2024-04-25 01:18:34 -0700

    search setting 8

--- components/modal/search-post-modal.tsx
+++ components/modal/search-post-modal.tsx
@@ -148,7 +148,7 @@
 
 "use client";
 
-import { Search } from "lucide-react";
+import { RotateCw, Search } from "lucide-react";
 import { ChangeEvent, useEffect, useState } from "react";
 
 import {
@@ -163,6 +163,9 @@ import { groq } from "next-sanity";
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import { useModalStore } from "@/hooks/useModalStore";
+import { searchQuery } from "@/sanity/lib/queries";
+import { urlForImage } from "@/sanity/lib/image";
+import Image from "next/image";
 const query = groq`
     *[_type == "post" && title match $value + "*"] | order(_createdAt desc){
   title,
@@ -176,9 +179,15 @@ type ItemProp = {
     current: string;
   };
   _id: string;
+  mainImage: any;
+  author: {
+    name: string;
+  };
 };
+
 export const SearchPostModal = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [items, setItems] = useState<ItemProp[]>([]);
 
   //   const { isOpen, onClose } = useSearch();
@@ -201,13 +210,23 @@ export const SearchPostModal = () => {
     event.stopPropagation();
     const value = event.target.value;
     if (value.length < 3) return;
-    const posts: ItemProp[] = await client.fetch(query, { value });
+    setIsLoading(true);
+    const posts: ItemProp[] = await client.fetch(searchQuery, {
+      value,
+      start: 0,
+      end: 6,
+    });
     setItems(posts);
+    setIsLoading(false);
   };
   return (
     <CommandDialog open={isModalOpen} onOpenChange={onClose}>
       <div className="flex items-center border-b px-3" cmdk-input-wrapper="">
-        <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
+        {isLoading ? (
+          <RotateCw className="mr-2 h-4 w-4 shrink-0 opacity-50 animate-spin" />
+        ) : (
+          <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
+        )}
         <Input
           onChange={handleChange}
           className="flex h-11 w-full rounded-md bg-transparent py-3 outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 border-none ring-offset-0 focus-visible:outline-0 focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
@@ -222,12 +241,24 @@ export const SearchPostModal = () => {
           )}
           {items?.map((item) => (
             <Link
-              href={`/post/${item.slug.current}`}
+              href={`/blog/${item.slug.current}`}
               key={item?._id}
               onClick={onClose}
             >
-              <CommandItem className="cursor-pointer">
-                {item?.title}
+              <CommandItem className="cursor-pointer border-b flex justify-between items-center gap-x-3">
+                <div className="flex flex-col">
+                  <p className="text-[1rem] line-clamp-1 font-semibold">
+                    {item.title}
+                  </p>
+                  <p className="text-xs">Author: {item.author.name}</p>
+                </div>
+                <div className="relative w-10 h-10">
+                  <Image
+                    src={urlForImage(item.mainImage)}
+                    alt="main image"
+                    fill
+                  />
+                </div>
               </CommandItem>
             </Link>
           ))}
